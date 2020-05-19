1fa529b542c98f7d5f394217f90d0edd7bc35670
Noarte <65566764+Noarte@users.noreply.github.com>
2020-05-19 15:35:20 +0300

    Update celadon_diner.asm

--- text/maps/celadon_diner.asm
+++ text/maps/celadon_diner.asm
@@ -1,60 +1,58 @@
 _CeladonDinerText1:: ; 9df3f (27:5f3f)
-	text "Hi!"
+	text "הי!"
 
-	para "We're taking a"
-	line "break now."
+	para "אנחנו בהפסקה"
+	line "כרגע."
 	done
 
 _CeladonDinerText2:: ; 9df5d (27:5f5d)
-	text "My #MON are"
-	line "weak, so I often"
-	cont "have to go to the"
-	cont "DRUG STORE."
+	text "הפוקימונים שלי"
+	line "חלשים אז אני"
+	cont "מבקר הרבה ב"
+	cont "DRUGה STORE." ;TODO
 	done
 
 _CeladonDinerText3:: ; 9df99 (27:5f99)
-	text "Psst! There's a"
-	line "basement under"
-	cont "the GAME CORNER."
+	text "פססס! יש מרתף נסתר"
+	line "מתחת ל GAME CORNER." ;TODO
 	done
 
 _CeladonDinerText4:: ; 9dfc9 (27:5fc9)
-	text "Munch..."
+	text "לעיסה..."
 
-	para "The man at that"
-	line "table lost it all"
-	cont "at the slots."
+	para "האיש הזה שליד השולחן"
+	line "איבד את כל מה שהיה לו"
+	cont "במכונות המזל הללו."
 	done
 
 _CeladonDinerText_491a7:: ; 9e003 (27:6003)
-	text "Go ahead! Laugh!"
+	text "לך על זה! חה חה!" ;TODO (הLaugh זה שיצחק או שהוא צוחק?)
 
-	para "I'm flat out"
-	line "busted!"
+	para "אני הובסתי"
+	line "לחלוטין!"
 
-	para "No more slots for"
-	line "me! I'm going"
-	cont "straight!"
+	para "סיימתי עם ההימורים"
+	line "אני הולך מעתה"
+	cont "בדרך הישר!"
 
-	para "Here! I won't be"
-	line "needing this any-"
-	cont "more!"
+	para "קח! אני לא"
+	line "אצטרך את זה יותר!"
 	prompt
 
 _ReceivedCoinCaseText:: ; 9e07a (27:607a)
-	text $52, " received"
+	text $52, " התקבל"
 	line "a @"
 	TX_RAM wcf4b
 	text "!@@"
 
 _CoinCaseNoRoomText:: ; 9e090 (27:6090)
-	text "Make room for"
-	line "this!"
+	text "תפנה קצת מקום"
+	line "בתיק שלך!"
 	done
 
 _CeladonDinerText_491b7:: ; 9e0a5 (27:60a5)
-	text "I always thought"
-	line "I was going to"
-	cont "win it back..."
+	text "תמיד חשבתי"
+	line "שאולי עוד אזכה"
+	cont "בכסף שלי בחזרה..."
 	done
 
