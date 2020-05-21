4fde87c5730ede93719061c877bbfcfb57f41879
mookie11 <65712424+mookie11@users.noreply.github.com>
2020-05-21 15:03:46 +0300

    Update daycare_1.asm

--- text/maps/daycare_1.asm
+++ text/maps/daycare_1.asm
@@ -1,65 +1,65 @@
 _DayCareMText_5640f:: ; 8ab95 (22:6b95)
-	text "I run a DAYCARE."
-	line "Would you like me"
-	cont "to raise one of"
-	cont "your #MON?"
+	text "אני מפעיל מעון #ימונים."
+	line "האם תרצה שאני"
+	cont "אגדל אחד"
+	cont "מה#ימונים שלך?"
 	done
 
 _DayCareMText_56414:: ; 8abd4 (22:6bd4)
-	text "Which #MON"
-	line "should I raise?"
+	text "איזה #ימון"
+	line "תרצה שאגדל לך?"
 	prompt
 
 _DayCareMText_56419:: ; 8abf0 (22:6bf0)
-	text "Fine, I'll look"
-	line "after @"
+	text "טוב, אני אשמור לך"
+	line "על @"
 	TX_RAM wcd6d
 	db $0
-	cont "for a while."
+	cont "לכמה זמן."
 	prompt
 
 _DayCareMText_5641e:: ; 8ac19 (22:6c19)
-	text "Come see me in"
-	line "a while."
+	text "תבוא לראות אותי"
+	line "עוד מעט."
 	done
 
 _DayCareMText_56423:: ; 8ac32 (22:6c32)
-	text "Your @"
+	text "ה@"
 	TX_RAM wcd6d
 	db $0
-	line "has grown a lot!"
+	line "גדל מאוד!"
 
-	para "By level, it's"
-	line "grown by @"
+	para "אם נדייק, הוא"
+	line "הוא גדל ב@ רמות"
 
 DayCareMText_8ac67:: ; 8ac67 (22:6c67)
 	TX_NUM wTrainerEngageDistance,$1,$3
 	text "!"
 
-	para "Aren't I great?"
+	para "נכון שאני אדיר?"
 	prompt
 
 _DayCareMText_56428:: ; 8ac7d (22:6c7d)
-	text "You owe me ¥@"
+	text "אתה חייב לי ¥@"
 	TX_BCD wcd3f, $c2
 	db $0
-	line "for the return"
-	cont "of this #MON."
+	line "על החזרתו"
+	cont "של ה#ימון הזה."
 	done
 
 _DayCareMText_5642d:: ; 8acae (22:6cae)
-	text $52, " got"
+	text $52, " קבל"
 	line "@"
 	TX_RAM W_DAYCAREMONNAME
-	text " back!"
+	text " בחזרה!"
 	done
 
 _DayCareMText_56432:: ; 8acc1 (22:6cc1)
-	text "Back already?"
-	line "Your @"
+	text "כבר חזרת?"
+	line "@ שלך"
 	TX_RAM wcd6d
 	db $0
-	cont "needs some more"
-	cont "time with me."
+	cont "צריך יותר"
+	cont "זמן איתי."
 	prompt
 
