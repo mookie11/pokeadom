58dc4dfb51db75643936dd09dd7f1219e802eb4b
mookie11 <65712424+mookie11@users.noreply.github.com>
2020-05-21 19:23:03 +0300

    Update indigo_plateau_lobby.asm

--- text/maps/indigo_plateau_lobby.asm
+++ text/maps/indigo_plateau_lobby.asm
@@ -1,26 +1,26 @@
 _IndigoPlateauLobbyText1:: ; a13f0 (28:53f0)
-	text "Yo! Champ in"
-	line "making!"
+	text "היי! אלוף"
+	line "בתחילת דרכו!"
 
-	para "At #MON LEAGUE,"
-	line "you have to face"
-	cont "the ELITE FOUR in"
-	cont "succession."
+	para "בליגת ה#ימונים,"
+	line "תצתרך להתמודד"
+	cont "מול הרבעייה העילית"
+	cont "ולנצח."
 
-	para "If you lose, you"
-	line "have to start all"
-	cont "over again! This"
-	cont "is it! Go for it!"
+	para "אם תפסיד, תצתרך"
+	line "להתחיל את הכל"
+	cont "מההתחלה. זהו זה!"
+	cont "לך תנצח את כולם!"
 	done
 
 _IndigoPlateauLobbyText3:: ; a148b (28:548b)
-	text "From here on, you"
-	line "face the ELITE"
-	cont "FOUR one by one!"
+	text "מפה והלאה, אתה"
+	line "תתמודד מול הרבעייה"
+	cont "העילית, כל אחד בתורו!"
 
-	para "If you win, a"
-	line "door opens to the"
-	cont "next trainer!"
-	cont "Good luck!"
+	para "אם תנצח, דלת"
+	line "תפתח ותעבור למאמן"
+	cont "הבא! בהצלחה"
+	cont "בהתמודדות!"
 	done
 
